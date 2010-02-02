81b0b0289b1c4d287ecc993f76046e30fe05be9b
theholydefender <theholydefender@gmail.com>
2010-02-02 23:28:34 +0000

    Created the SQlite database within the MintData class.  Added a Constants interface to allow for one consistant place for table and column names.

---
+++ MintTrack/src/org/ponyinc/minttrack/Constants.java
@@ -0,0 +1,28 @@
+package org.ponyinc.minttrack;
+
+import android.provider.BaseColumns;
+
+public interface Constants extends BaseColumns{
+	public static final String ACCOUNT_TBLNAM = "account";
+	public static final String CATEGORY_TBLNAM = "categorie";
+	public static final String TRANSACTION_TBLNAM = "tran_table";
+
+	// Columns in the Accounts Table
+	public static final String ACCOUNT_NAME = "account_name";
+	public static final String ACCOUNT_TOTAL = "account_total";
+	
+	//Columns in the Category Table
+	public static final String CATEGORY_NAME = "category_name";
+	public static final String CATEGORY_TOTAL = "category_total";
+	public static final String CATEGORY_TYPE = "category_type";
+	
+	//Columns in the Transaction Table
+	public static final String TRANSACTION_DATE = "tran_date";
+	public static final String TRANSACTION_TOACCOUNT = "tran_toaccount";
+	public static final String TRANSACTION_FROMACCOUNT = "tran_fromaccount";
+	public static final String TRANSACTION_AMOUNT = "tran_amount";
+	public static final String TRANSACTION_CATEGORY = "tran_category";
+	public static final String TRANSACTION_NOTE = "tran_note";
+	public static final String TRANSACTION_TYPE = "tran_type";
+	
+}
--- MintTrack/src/org/ponyinc/minttrack/MintTrack.java
+++ MintTrack/src/org/ponyinc/minttrack/MintTrack.java
@@ -1,10 +1,18 @@
 package org.ponyinc.minttrack;
 
+import static android.provider.BaseColumns._ID;
+import static org.ponyinc.minttrack.Constants.*;
+
 import java.util.Calendar;
 
+import org.ponyinc.minttrack.MintData;
+import org.ponyinc.minttrack.R;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TabActivity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -18,12 +26,15 @@ import android.widget.TextView;
 public class MintTrack extends TabActivity {
     /** Called when the activity is first created. */
 	TabHost mTabHost;
+	private MintData MintLink;
 	private TextView mDateDisplay;
     private Button mPickDate;
     private int mYear;
     private int mMonth;
     private int mDay;
     static final int DATE_DIALOG_ID = 0;
+    private static String[] FROM = { _ID, ACCOUNT_NAME, ACCOUNT_TOTAL, };
+    private static String ORDER_BY = ACCOUNT_NAME + " DESC";
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +49,16 @@ public class MintTrack extends TabActivity {
         mTabHost.addTab(mTabHost.newTabSpec("Tools").setIndicator("", getResources().getDrawable(R.drawable.toolsbtn)).setContent(R.id.tab4));
         mTabHost.setCurrentTab(0);
         
+        MintLink = new MintData(this);
+        
+        try{
+        	addAccount("Pentucket Bank", 5000.67);
+        	addAccount("Boston Bank", 5000.00);
+        	Cursor cursor = getAccounts();
+        	showEvents(cursor);
+        } finally {
+        	MintLink.close();
+        }
         //Button
         final ImageButton button = (ImageButton) findViewById(R.id.android_button);
         
@@ -113,5 +134,40 @@ public class MintTrack extends TabActivity {
                                 mYear, mMonth, mDay);
                 }
                 return null;
+            }
+            private void addAccount(String strName, double initalValue){
+            	//Insert a new record into the Events data source.
+            	//You would do something similar for delete and update
+            	SQLiteDatabase db = MintLink.getWritableDatabase();
+            	ContentValues values = new ContentValues();
+            	/*
+            	public static final String ACCOUNT_NAME = "account_name";
+            	public static final String ACCOUNT_TOTAL = "account_total";
+            	*/
+            	values.put(ACCOUNT_NAME, strName);
+            	values.put(ACCOUNT_TOTAL, initalValue);
+            	db.insertOrThrow(ACCOUNT_TBLNAM, null, values);
+            }
+            
+            private Cursor getAccounts() {
+            	SQLiteDatabase db = MintLink.getReadableDatabase();
+            	Cursor cursor = db.query(ACCOUNT_TBLNAM, FROM, null, null, null, null, ORDER_BY);
+            	startManagingCursor(cursor);
+            	return cursor;
+            }
+            
+            private void showEvents(Cursor cursor){
+            	StringBuilder builder = new StringBuilder("Saved Accounts:\n");
+            	while (cursor.moveToNext()){
+            		long id = cursor.getLong(0);
+            		String name = cursor.getString(1);
+            		float total = cursor.getFloat(2);
+            		builder.append(id).append(": ");
+            		builder.append(name).append(": ");
+            		builder.append(total).append("\n");
+            	}
+            	
+            	TextView text = (TextView) findViewById(R.id.text);
+            	text.setText(builder);
             }
 }
