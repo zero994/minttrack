c789c0acd2eea617eac795b09da995ca42b771a2
jeff.rtitus@gmail.com <jeff.rtitus@gmail.com@df803076-06cb-11df-adb7-1be7f7e3f636>
2010-02-21 05:44:23 +0000

    Changes to HomeActivity.java and home.xml Small change: added transfer button to entry.xml

--- branches/MintTrackV2/MintTrack/src/com/ponyinc/minttrack/HomeActivity.java
+++ branches/MintTrackV2/MintTrack/src/com/ponyinc/minttrack/HomeActivity.java
@@ -1,19 +1,31 @@
 package com.ponyinc.minttrack;
 
+import static android.provider.BaseColumns._ID;
+import static com.ponyinc.minttrack.Constants.TRANSACTION_AMOUNT;
+import static com.ponyinc.minttrack.Constants.TRANSACTION_NOTE;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends ListActivity {
 
 	Budget budget;
 
 	public void onCreate(Bundle savedInstanceState) {
+		Cursor translist;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
 
 		budget = new Budget(this);
+		try{
+			budget.Transfer(1,1,2123.33, "Testing", "01022010");
+			translist = budget.getTransactions();
+			showEvents(translist);
+		}
+		finally{}
 		
 		Cursor AccountCursor =  budget.getAccounts();
 		Cursor CategoryCursor = budget.getCategorys();
@@ -28,5 +40,15 @@ public class HomeActivity extends Activity {
 		
 		CategoryCursor.close();
 		AccountCursor.close();
+	}
+	
+	private static int[] TO = { R.id.rowid, R.id.time, R.id.title, };
+	private static String[] FROM = { _ID, TRANSACTION_AMOUNT, TRANSACTION_NOTE, };
+	
+	private void showEvents(Cursor cursor) {
+		// Set up data binding
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+				R.layout.audititem, cursor, FROM, TO);
+		setListAdapter(adapter);
 	}
 }
