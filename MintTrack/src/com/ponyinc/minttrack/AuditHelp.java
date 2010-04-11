574f838df9ee4972be29d23b9b72dc26dab4e2af
pbajo88 <pbajo88@gmail.com>
2010-04-11 18:54:58 +0000

    audit help finished

---
+++ MintTrack/src/com/ponyinc/minttrack/AuditHelp.java
@@ -0,0 +1,32 @@
+
+package com.ponyinc.minttrack;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+public class AuditHelp extends Activity
+{
+	 public void onCreate(Bundle savedInstanceState) 
+	    {
+	       super.onCreate(savedInstanceState);
+	       setContentView(R.layout.helpaudit);
+	       TextView text = (TextView)findViewById(R.id.help1);
+	       TextView text2 = (TextView)findViewById(R.id.help2);
+	       TextView text3 = (TextView)findViewById(R.id.help3);
+	       TextView text4 = (TextView)findViewById(R.id.help4);
+	       
+	       text.setText("Help for Audit Tab");
+	       text2.setText("Components: All the transactions");
+	       text3.setText("·Transactions items");
+	       text4.setText("Date - the date of the transaction.\n"+
+	       		"Type - the type of the transaction.\n"+
+	       		"Amount - the amount of the transaction.\n"+
+	       		"Reason - the reason of the transaction.\n"+
+	       		"Note - a complementary note of the transaction.");
+	       
+	    }
+	
+}
