00724fc9c8187523e7a781bd9c750447adf20d7a
jeff.rtitus@gmail.com <jeff.rtitus@gmail.com@df803076-06cb-11df-adb7-1be7f7e3f636>
2011-02-12 05:13:44 +0000

    Fixed force close error

--- branches/MintTrackV2/MintTrack/src/com/ponyinc/minttrack/tools/TipCalculator.java
+++ branches/MintTrackV2/MintTrack/src/com/ponyinc/minttrack/tools/TipCalculator.java
@@ -1,5 +1,5 @@
 /**This class implements the Tip Calculator when called from the Tip Calculator button in the Tools Tab
- * @author Pablo BajoLaso and Jeff Titus
+ * @author Jeff Titus
  */
 package com.ponyinc.minttrack.tools;
 
@@ -17,7 +17,7 @@ import android.widget.TextView;
 
 public class TipCalculator extends Activity
 {
-	EditText bill;
+    EditText bill;
     EditText tip;
     EditText split;
     TextView totalCheck;
@@ -49,20 +49,35 @@ public class TipCalculator extends Activity
      * @return true if string is OK, or false if not
      */
     public boolean isValid(String str){
-    	for(int c = 0; c < str.length(); c++){
-    		if((str.charAt(c) <= 46 || str.charAt(c) >= 58) && (str.charAt(c) != '.'))
-    			return false;
-    	}
-    	return true;
+		if(str.contains(".") && str.indexOf('.') < str.lastIndexOf('.'))
+			return false;
+        for(int c = 0; c < str.length(); c++){
+                if((str.charAt(c) <= 46 || str.charAt(c) >= 58) && (str.charAt(c) != '.'))
+                        return false;
+        }
+        return true;
     }
 	
+	/**
+     * Checks to see if str is valid input
+     * @param str String entered by the user
+     * @return true if string is OK, or false if not
+     */
+    public boolean isValidSplitValue(String str){
+        for(int c = 0; c < str.length(); c++){
+                if((str.charAt(c) <= 46 || str.charAt(c) >= 58))
+                        return false;
+        }
+        return true;
+    }
+        
    /**OnClickListener for Execute button**/
    View.OnClickListener executeListener = new View.OnClickListener()
-   {	   
-	   @Override
+   {       
+           @Override
        public void onClick(View v) 
-       {		   
-    	   NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
+       {                   
+           NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
            
            String s_bill= bill.getText().toString();
            String s_tip= tip.getText().toString();
@@ -77,46 +92,46 @@ public class TipCalculator extends Activity
 			   split.setText(s_split);
 		   }
            if(s_bill.equalsIgnoreCase("")){
-        	   errorString.setText("X Please enter a value for the bill.");
+                errorString.setText("X Please enter a value for the bill.");
 		   }
            else if (!isValid(s_bill)){
-        	   errorString.setText("X Bill input is not valid.");
+                errorString.setText("X Bill input is not valid.");
            }
 		   else if (!isValid(s_tip)){
-        	   errorString.setText("X Tip input is not valid.");
+				errorString.setText("X Tip input is not valid.");
            }
-		   else if (!isValid(s_split)){
-        	   errorString.setText("X Split input is not valid.");
-			   
+		   else if (!isValidSplitValue(s_split)){
+                errorString.setText("X Split input is not valid.");
+                           
            }
            else{
 			   errorString.setText("");
-	           float f_bill= new Float(s_bill).floatValue(); 
-	           float f_tip= new Float(s_tip).floatValue();
-	           float f_split= new Float(s_split).floatValue();
-	           double d_total=(f_bill+(f_bill*f_tip/100));
-	           double d_perPerson = d_total/f_split;
-	           // show the string in total editText	        
-	           String s_totalBill = n.format(d_total);
-	           String s_totalPerPerson = n.format(d_perPerson);
-	           totalCheck.setText(s_totalBill);
-	           checkPerPerson.setText(s_totalPerPerson);
+			   float f_bill= new Float(s_bill).floatValue(); 
+			   float f_tip= new Float(s_tip).floatValue();
+			   int i_split= Integer.parseInt(s_split);
+			   double d_total=(f_bill+(f_bill*f_tip/100));
+			   double d_perPerson = d_total/(float)i_split;
+			   // show the string in total editText
+			   String s_totalBill = n.format(d_total);
+			   String s_totalPerPerson = n.format(d_perPerson);
+			   totalCheck.setText(s_totalBill);
+			   checkPerPerson.setText(s_totalPerPerson);
 		   }
        }
    };
    
    /**OnClickListener for Clear button**/
    View.OnClickListener clearListener = new View.OnClickListener()
-   {	   
-	   @Override
+   {       
+           @Override
        public void onClick(View v) 
-       {		   
-		    errorString.setText("");
-		    bill.setText("");
-	 	  	tip.setText("");
-	 	  	split.setText("");
-	 	  	totalCheck.setText("");
-	 	  	checkPerPerson.setText("");
+       {                   
+			errorString.setText("");
+			bill.setText("");
+			tip.setText("");
+			split.setText("");
+			totalCheck.setText("");
+			checkPerPerson.setText("");
        }
    };
 }
